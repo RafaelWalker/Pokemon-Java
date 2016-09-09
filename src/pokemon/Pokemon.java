887b2c1f8ce8ba57ff6adc00a3b62187ce6facd4
Rafael Walker Batista <rafaelwalker@icloud.com>
2016-09-09 16:00:05 -0300

    First commit Pokemon e PokemonAction

---
+++ src/pokemon/Pokemon.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pokemon;
+
+/**
+ *
+ * @author rafael
+ */
+public abstract class Pokemon implements PokemonAction{
+
+    private final String nome;
+    private int hp;
+    private final int cp;
+    private final int poderAtaque;
+    private String tipo;
+
+    public Pokemon(String nome, int hp, int cp) {
+        this.nome = nome;
+        setHp(hp);
+        this.cp = cp;
+        this.tipo = tipo;
+        this.poderAtaque = calculaPoderAtaque();
+        
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+
+//    public void setNome(String nome) {
+//        this.nome = nome;
+//    }
+
+    public int getHp() {
+        return hp;
+    }
+
+   public void setHp(int hp) {
+      this.hp = hp;
+  }
+
+    public int getCp() {
+        return cp;
+    }
+
+//    public void setCp(int cp) {
+//        this.cp = cp;
+//    }
+
+    public int getPoderAtaque() {
+        return poderAtaque;
+    }
+
+//    public void setPoderAtaque(int poderAtaque) {
+//        this.poderAtaque = poderAtaque;
+//    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+//    public void setTipo(String tipo) {
+//        this.tipo = tipo;
+//    }
+        
+    public abstract int calculaPoderAtaque();
+}
+
+    
+
