336a2e5e658efffb82993e1f6c6a4359c5270883
Rafael Walker Batista <rafaelwalker@icloud.com>
2016-09-14 19:39:24 -0300

    alteraçoes classe view

--- src/pokemon/View.java
+++ src/pokemon/View.java
@@ -2,17 +2,43 @@ package pokemon;
 
 import Utilidade.Util;
 import java.time.LocalDate;
+
 public class View {
-    public void menu(){
-               
-        int opcao= 0;
+
+    public void menu() {
+
+        int opcao = 0;
         do {
-            System.out.println("\nPokemon");
+            System.out.println("Pokemon");
             System.out.println("--------------------------------");
-            System.out.println("1. Pokemon de Água");
-            System.out.println("2. Pokemon de Fogo");
-            System.out.println("3. Pokemon de Terra");
-            System.out.println("4. Pokemon de Eletricidade");
-                  try {
-                escolha = Util.leInteiro("Escolha uma opção: ");
-                
+            System.out.println("1. Pokemon de Água              ");
+            System.out.println("2. Pokemon de Fogo              ");
+            System.out.println("3. Pokemon de Terra             ");
+            System.out.println("4. Pokemon de Eletricidade      ");
+            System.out.println("0. Para Sair                    ");
+            System.out.println("--------------------------------");
+            
+            try {
+                opcao = Util.leInteiro("Escolha uma opção: ");
+                switch (opcao) {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 0:
+                        break;
+                    default:
+                        System.out.println("Opção Inválida!");
+                }
+            } catch (Exception e){
+
+                System.out.println(e.getMessage());
+            } 
+        } while (opcao != 0);
+    }
+}
+ 
